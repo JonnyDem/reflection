5a34cd7336de7c5f21a438e4f22e690ffad6a298
JonnyDem <tl-k@ukr.net>
2017-09-14 16:55:59 +0300

    changed

--- src/main/java/reflection/HackService.java
+++ src/main/java/reflection/HackService.java
@@ -4,81 +4,79 @@ package reflection;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class HackService {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException, InvocationTargetException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException, InvocationTargetException, NoSuchMethodException {
         hackFieldsAndArray();
         hackMethods();
     }
     private static void hackFieldsAndArray() throws IllegalAccessException, NoSuchFieldException {
         User user = new User();
         Class<? extends User> userClass = user.getClass();
-        Field[] declaredFields = userClass.getDeclaredFields();
-        for(Field f:declaredFields){
-            System.out.println("name: " + f.getName() + ", type: " + f.getType());
-            f.setAccessible(true);
-            Object o = f.get(user);
-            if(f.getType().isArray()){
-                Field field = userClass.getDeclaredField("array");
-                field.setAccessible(true);
-                int[] array = (int[]) field.get(user);
-                for(int i:array){
-                    System.out.println("before changing: " + i);
-                }
-                array[1] = 9876;
-                int[] newArray = (int[]) field.get(user);
-                System.out.println();
-                for(int i:newArray){
-                    System.out.println("after changing: " + i);
-                }
-                System.out.println();
-            }
-            if(f.getName().equals("name")&&o instanceof String){
-                Field field = userClass.getDeclaredField("name");
-                System.out.println("before changing: " + o);
-                field.setAccessible(true);
-                field.set(user,"John");
-                String str = (String) f.get(user);
-                System.out.println("after changing: " + str);
-                System.out.println();
-            }
-            if(f.getName().equals("surname")&&o==null){
-                Field field = userClass.getDeclaredField("surname");
-                System.out.println("before changing: " + o);
-                field.setAccessible(true);
-                field.set(user,"Smith");
-                String str = (String) f.get(user);
-                System.out.println("after changing: " + str);
-                System.out.println();
-            }
-            if(f.getName().equals("id")&&o instanceof Integer){
-                Field field = userClass.getDeclaredField("id");
-                System.out.println("before changing: " + o);
-                field.setAccessible(true);
-                field.set(user,987);
-                int newId = (Integer) f.get(user);
-                System.out.println("after changing: " + newId);
-                System.out.println();
-            }
-            if(f.getName().equals("age")&&o instanceof Integer){
-                Field field = userClass.getDeclaredField("age");
-                System.out.println("before changing: " + o);
-                field.setAccessible(true);
-                field.set(user,47);
-                int newAge = (Integer) f.get(user);
-                System.out.println("after changing: " + newAge);
-                System.out.println();
-            }
-        }
+        Field nameField = userClass.getDeclaredField("name");
+        nameField.setAccessible(true);
+        System.out.println("name: " + nameField.getName() + ", type: " + nameField.getType());
+        Object o = nameField.get(user);
+        System.out.println("before changing: " + o);
+        nameField.set(user,"John");
+        o = nameField.get(user);
+        System.out.println("after changing: " + o);
+        System.out.println();
+
+        Field surnameField = userClass.getDeclaredField("surname");
+        surnameField.setAccessible(true);
+        System.out.println("name: " + surnameField.getName() + ", type: " +surnameField.getType());
+        o = surnameField.get(user);
+        System.out.println("before changing: " + o);
+        surnameField.set(user,"Smith");
+        o = surnameField.get(user);
+        System.out.println("after changing: " + o);
+        System.out.println();
+
+        Field idField = userClass.getDeclaredField("id");
+        idField.setAccessible(true);
+        System.out.println("name: " + idField.getName() + ", type: " + idField.getType());
+        o = idField.get(user);
+        System.out.println("before changing: " + o);
+        idField.set(user,987);
+        o = idField.get(user);
+        System.out.println("after changing: " + o);
+        System.out.println();
+
+        Field ageField = userClass.getDeclaredField("age");
+        ageField.setAccessible(true);
+        System.out.println("name: " + ageField.getName() + ", type: " + ageField.getType());
+        o = ageField.get(user);
+        System.out.println("before changing: " + o);
+        ageField.set(user,76);
+        o = ageField.get(user);
+        System.out.println("after changing: " + o);
+        System.out.println();
+
+        Field arrayField = userClass.getDeclaredField("array");
+        arrayField.setAccessible(true);
+        System.out.println("name: " + arrayField.getName() + ", array: " + arrayField.getType().isArray());
+        int[] array = (int[]) arrayField.get(user);
+        System.out.println("before changing: " + Arrays.toString(array));
+        array[1] = 123456;
+        int[] newArray = (int[]) arrayField.get(user);
+        System.out.println("after changing: " + Arrays.toString(newArray));
+        System.out.println();
+
     }
-    private static void hackMethods() throws InvocationTargetException, IllegalAccessException {
+
+    private static void hackMethods() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         User user = new User();
         Class<? extends User> userClass = user.getClass();
-        Method[] methods = userClass.getDeclaredMethods();
-        for(Method m: methods){
-             m.setAccessible(true);
-            System.out.println("method: " + m.getName() + " invokes: " + m.invoke(user));
+        Method method = userClass.getDeclaredMethod("job");
+        method.setAccessible(true);
+        System.out.println("method: " + method.getName() + " invokes: " + method.invoke(user));
+
+        method = userClass.getDeclaredMethod("getAge");
+        method.setAccessible(true);
+        System.out.println("method: " + method.getName() + " invokes: " + method.invoke(user));
         }
     }
-}
+
 
